809b5d4fe5dc02ebd57ba3be4c5866a52582eeb0
Marcos Lima <marcoslima@Tycho25.local>
2025-12-28 11:30:35 -0300

    app

--- app.R
+++ app.R
@@ -1,4 +1,11 @@
 
-library(gaivotapp)
-gaivotapp::run_app()
+# library(gaivotapp)
+# gaivotapp::run_app()
+
+
+# app.R
+# Works when the package is NOT installed (Shiny mode)
+
+pkgload::load_all(export_all = FALSE)
+run_app()
 
